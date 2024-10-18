0e4c34d61dbe42b110f90366bebf91349da54a23
Juan Cuenca <juan99.cuenca@gmail.com>
2024-10-18 16:38:37 -0300

    update fonts and service text

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,12 @@ function App() {
         <main>
           <section id="home" className="px-10 sm:px-20 md:px-40 py-10 md:py-20 flex flex-col lg:flex-row justify-between gap-8">            
             <div className='max-w-lg text-center lg:text-left'>
-                <h1 className='text-4xl md:text-5xl font-bold text-gray-900 leading-tight tracking-normal'>A&C Soluciones Informáticas</h1>
+                <h1 className='text-4xl md:text-5xl font-bold text-gray-950 leading-tight tracking-normal'>A&C Soluciones Informáticas</h1>
                 <p className='text-lg text-gray-600 text-balance mt-6 tracking-wide'>
                   Bienvenidos a AC Soluciones Informáticas, tu aliado para todas tus necesidades tecnológicas. Ofrecemos una amplia gama de servicios que van desde la creación de trabajos universitarios hasta el soporte técnico especializado para tu equipo. Contáctanos para una solución rápida y profesional.
                 </p>
                 <a href="https://wa.me/+595995680264" target="_blank">  
-                  <button className='mt-8 px-5 py-2 border bg-gray-900 text-white w-fit rounded-md text-lg font-semibold hover:bg-white hover:text-gray-900 hover:border-gray-900'>Contactar Ahora</button>
+                  <button className='mt-8 px-5 py-2 border bg-gray-950 text-white w-fit rounded-md text-lg font-semibold hover:bg-white hover:text-gray-950 hover:border-gray-900'>Contactar Ahora</button>
                 </a>
             </div>
             <div className='flex justify-center lg:block'>
@@ -29,41 +29,41 @@ function App() {
             </div>
           </section>
           <section id="servicios" className="px-10 sm:px-20 md:px-40 py-10 md:py-20 bg-slate-100" >
-              <h1 className='text-4xl md:text-5xl font-bold text-gray-900 leading-tight tracking-normal text-center lg:text-left'>Servicios</h1>
+              <h1 className='text-4xl md:text-5xl font-bold text-gray-950 leading-tight tracking-normal text-center lg:text-left'>Servicios</h1>
               <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 mt-14 lg:mt-16'>
                 <div>
-                  {/* <h2 className='text-2xl text-gray-900 font-bold'>Trabajos universitarios</h2> */}
+                  {/* <h2 className='text-2xl text-gray-950 font-bold'>Trabajos universitarios</h2> */}
                   <div className='flex gap-x-4'>
                     <div className='w-1 h-8 bg-red-600'></div>
-                    <h2 className='text-2xl text-gray-900 font-bold'>Trabajos Universitarios</h2>
+                    <h2 className='text-2xl text-gray-950 font-bold'>Trabajos Universitarios</h2>
                   </div>
                   <p className='text-base text-gray-600 mt-3'>
-                    Realizamos trabajos académicos como ensayos, monografías, y proyectos de investigación. Además, te ayudamos a crear presentaciones en PowerPoint que capturen la atención de tu audiencia.
+                    Realizamos trabajos académicos como ensayos, monografías, proyectos de investigación, presentaciones y más. Además, te ayudamos a crear presentaciones en PowerPoint que capturen la atención de tu audiencia.
                   </p>
                 </div>
                 <div>
-                  {/* <h2 className='text-2xl text-gray-900 font-bold'>Diseño Gráfico</h2> */}
+                  {/* <h2 className='text-2xl text-gray-950 font-bold'>Diseño Gráfico</h2> */}
                   <div className='flex gap-x-4'>
                     <div className='w-1 h-8 bg-red-600'></div>
-                    <h2 className='text-2xl text-gray-900 font-bold'>Diseño Gráfico</h2>
+                    <h2 className='text-2xl text-gray-950 font-bold'>Diseño Gráfico</h2>
                   </div>
                   <p className='text-base text-gray-600 mt-3'>
-                    Creamos flyers, currículums profesionales, tarjetas de presentación y más. Nos aseguramos de que cada diseño sea moderno y esté alineado con tus expectativas.
+                    Creamos flyers, logos, catálogos de productos, videos informativos, currículums profesionales, tarjetas de presentación y más. Nos aseguramos de que cada diseño sea moderno y esté alineado con tus expectativas.
                   </p>
                 </div>
                 <div>
                   <div className='flex gap-x-4'>
                     <div className='w-1 h-8 bg-red-600'></div>
-                    <h2 className='text-2xl text-gray-900 font-bold'>Soporte Técnico</h2>
+                    <h2 className='text-2xl text-gray-950 font-bold'>Soporte Técnico</h2>
                   </div>
                   <p className='text-base text-gray-600 mt-3'>
-                    Ofrecemos mantenimiento preventivo, limpieza, formateo, instalación de software y paquetes como Office, además de recuperación de archivos perdidos. Tu equipo estará en buenas manos con nosotros.
+                    Ofrecemos dianostico completo, mantenimiento preventivo, limpieza de sistema y optimización, formateo, instalación de software y paquetes como Office, además de recuperación de archivos perdidos. Tu equipo estará en buenas manos con nosotros.
                   </p>
                 </div>
               </div>
           </section>
           <section id='proyectos' className="px-10 sm:px-20 md:px-40 py-10 md:py-20">
-            <h1 className='text-4xl md:text-5xl font-bold text-gray-900 leading-tight tracking-normal text-center lg:text-left'>Nuestros Trabajos</h1>
+            <h1 className='text-4xl md:text-5xl font-bold text-gray-950 leading-tight tracking-normal text-center lg:text-left'>Nuestros Trabajos</h1>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-8 mt-14 lg:mt-16'>
                 <div>
                   <img src={imagen8} alt="imagen" className='rounded-sm' />
