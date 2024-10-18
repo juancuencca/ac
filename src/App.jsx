6a52ee5de2819dd42a29b52059f3a8b2f69c2a0c
Juan Cuenca <juan99.cuenca@gmail.com>
2024-10-18 08:29:10 -0300

    change hero paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,7 @@ function App() {
             <div className='max-w-lg'>
                 <h1 className='text-5xl font-bold text-gray-900 leading-tight tracking-normal'>A&C Soluciones Informáticas</h1>
                 <p className='text-lg text-gray-600 text-balance mt-6 tracking-wide'>
-                  Ofrecemos soluciones tecnológicas eficientes y personalizadas, 
-                  adaptándonos a tus necesidades específicas, 
-                  asegurando un servicio confiable y de calidad.
+                Bienvenidos a AC Soluciones Informáticas, tu aliado para todas tus necesidades tecnológicas. Ofrecemos una amplia gama de servicios que van desde la creación de trabajos universitarios hasta el soporte técnico especializado para tu equipo. Contáctanos para una solución rápida y profesional.
                 </p>
                 <button className='mt-8 px-5 py-2 border bg-gray-900 text-white w-fit rounded-md text-lg font-semibold hover:bg-white hover:text-gray-900 hover:border-gray-900'>Contáctanos</button>
             </div>
